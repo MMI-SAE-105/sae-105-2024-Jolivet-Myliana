350facde248f455b1acc298bc40b1afe50630519
Jolivet-Myliana <myliana.jolivet@edu.univ-fcomte.fr>
2024-12-11 13:37:59 +0100

    ajout click au bouton du carousel

--- src/js/page.js
+++ src/js/page.js
@@ -2,13 +2,14 @@ const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
+
 if (carousel) {
 prevButton.addEventListener("click", () => {
-const scrollAmount = premierItem.clientWidth;
 carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
 });
+
 nextButton.addEventListener("click", () => {
-const scrollAmount = premierItem.clientWidth;
 carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
 });
 }
