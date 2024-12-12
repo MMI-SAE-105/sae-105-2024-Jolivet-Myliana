4159abb446fb2a2a43d83b86691c49f378912c03
Jolivet-Myliana <myliana.jolivet@edu.univ-fcomte.fr>
2024-12-13 00:06:41 +0100

    page 3

---
+++ src/js/pageFAQ.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector(".header__action--menu");
+const nav = document.querySelector(".header__nav");
+
+// Gestion du clic sur le bouton du menu
+toggle.addEventListener("click", () => {
+    // Ajout ou suppression de la classe pour afficher ou masquer le menu
+    nav.classList.toggle("header__nav--open");
+
+    // Mise à jour des attributs ARIA pour l'accessibilité
+    const isExpanded = toggle.getAttribute("aria-expanded") === "true";
+    toggle.setAttribute("aria-expanded", !isExpanded);
+    nav.setAttribute("aria-hidden", isExpanded);
+});
+
