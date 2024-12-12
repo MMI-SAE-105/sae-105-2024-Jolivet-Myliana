c131a2ca98bcee37ac19ef016ccf139f167f53bd
Jolivet-Myliana <myliana.jolivet@edu.univ-fcomte.fr>
2024-12-12 23:56:25 +0100

    html et css et jspage 2

--- src/js/page.js
+++ src/js/page.js
@@ -4,6 +4,7 @@ const nextButton = document.querySelector(".carousel__button--next");
 const premierItem = document.querySelector(".carousel__item");
 const scrollAmount = premierItem.clientWidth;
 
+
 if (carousel) {
 prevButton.addEventListener("click", () => {
 carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
